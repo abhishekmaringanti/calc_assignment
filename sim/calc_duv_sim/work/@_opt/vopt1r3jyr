ba89067a31560a8f810df67c54347c639f8ad258
Abhishek Maringanti <am17645@snowy.cs.bris.ac.uk>
2017-10-17 18:44:17 +0100

    deleted non git files

--- sim/calc_duv_sim/work/@_opt/vopt1r3jyr
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity calc1_tb_top is
-end calc1_tb_top;
--- sim/calc_duv_sim/work/gen_clk_rst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity gen_clk_rst is
-    port(
-        tb_clk_out      : out    vl_logic;
-        tb_rst_out      : out    vl_logic_vector(1 to 7)
-    );
-end gen_clk_rst;
